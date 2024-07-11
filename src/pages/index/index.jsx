8cb5df954d58d917735da87a6f3acf4bb99e8da9
yingjieweb <yingjieweb@qq.com>
2024-07-11 20:03:41 +0800

    feat: 🎸 Add ScrollView demo code

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,57 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text, ScrollView } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
+import "./index.scss";
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
 
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+
+      <ScrollView
+        scrollY
+        enhanced
+        style={{ height: `20vh`, border: "1px solid red;" }}
+        onScroll={(e) => {
+          console.log("onScroll", e);
+          Taro.showToast({
+            title: "onScroll",
+            icon: "none",
+          });
+        }}
+        onScrollStart={() => {
+          console.log("onScrollStart");
+          Taro.showToast({
+            title: "onScrollStart",
+            icon: "none",
+          });
+        }}
+        onScrollEnd={() => {
+          console.log("onScrollEnd");
+          Taro.showToast({
+            title: "onScrollEnd",
+            icon: "none",
+          });
+        }}
+      >
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+        <View>Scroll me up</View>
+      </ScrollView>
     </View>
-  )
+  );
 }
